6721078cfcbacc48159c0a1ad3de4e28fa5c994a
김동환 <1033513@naver.com>
2021-04-12 20:23:57 +0900

    Initial Commit

---
+++ scroll_tutorial/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  scroll_tutorial
+//
+//  Created by 김동환 on 2021/04/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    //뷰의 height 정보가 없어도, label의 자체 크기로 height 설정 됨
+    
+    
+}
+
